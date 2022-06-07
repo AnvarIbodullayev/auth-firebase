201dda249057e976bc5c08548feeab4fe9e963e9
Anvar <61209846+AnvarIbodullayev@users.noreply.github.com>
2022-06-08 01:47:02 +0500

    Add files via upload

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { UserAuth } from '../../context/AuthContext'
+import { UserAuth } from '../../context/AuthContext';
+import  anonymousUserImage from './Anonymous_emblem.jpg';
 
 export default function Account() {
     const { user, logout } = UserAuth();
@@ -19,7 +20,11 @@ export default function Account() {
     return (
         <div className='max-w-[600px] mx-auto my-16 p-4'>
             <h1 className='text-2xl font-bold py-4'>Account</h1>
-            <p>{ user.diplayName ? ` ` : `Name: ${user && user.displayName}` }</p>
+            <img className="w-24 h-24 rounded-full" src={
+                user.photoURL === null ? anonymousUserImage : user && user.photoURL
+            } alt="" width="384" height="512"></img>
+            
+            <p>{ user.displayName === null ? 'Name: Anonymous user' : `Name: ${user && user.displayName}` }</p>
             
             <p>User Email: {user && user.email}</p>
             <button onClick={handleLogout} className='border px-6 py-2 my-4'>Logout</button>
